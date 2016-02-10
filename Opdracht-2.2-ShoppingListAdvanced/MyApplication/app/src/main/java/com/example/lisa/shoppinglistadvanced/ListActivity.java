ae418b82dd986e688f7ae35a4a64fb407fae0f18
Lisa The <lisathe97@gmail.com>
2016-02-10 21:59:33 +0100

    Step 5: Deleting items

--- Opdracht-2.2-ShoppingListAdvanced/MyApplication/app/src/main/java/com/example/lisa/shoppinglistadvanced/ListActivity.java
+++ Opdracht-2.2-ShoppingListAdvanced/MyApplication/app/src/main/java/com/example/lisa/shoppinglistadvanced/ListActivity.java
@@ -1,11 +1,13 @@
 package com.example.lisa.shoppinglistadvanced;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +36,7 @@ public class ListActivity extends AppCompatActivity {
         //Create list of items
         items = new ArrayList<ListItem>();
 
+        registerForContextMenu(listView);
         adapter = new ItemAdapter(this,R.layout.row_item,items);
 
         items.add(new ListItem("Apple", "Granny Smith", R.mipmap.ic_launcher));
@@ -69,6 +72,35 @@ public class ListActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+
+        //Get the clicked item
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+
+        //Inflate the context menu from the resource file
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+
+        //Find the delete MenuItem by its ID
+        MenuItem deleteButton = menu.findItem(R.id.context_menu_delete_item);
+        super.onCreateContextMenu(menu, view, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        //Retrieve info about the long pressed list item
+        AdapterView.AdapterContextMenuInfo itemInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        if (item.getItemId() == R.id.context_menu_delete_item) {
+            //Remove the item from the list
+            items.remove(itemInfo.position);
+            //Update the adapter to reflect the list change
+            adapter.notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_list, menu);
@@ -86,7 +118,38 @@ public class ListActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (item.getItemId() == R.id.action_bar_menu_delete_all) {
+        //Clears the list
+            items.clear();
+
+        //Tell the adapter that it should reload the data
+            adapter.notifyDataSetChanged();
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Check if the result code is the right one
+        if (resultCode == Activity.RESULT_OK) {
+            //Check if the request code is correct
+            if (requestCode == 1234) {
+                //Everything's fine, get the values;
+                String title = data.getStringExtra("title");
+                String description = data.getStringExtra("description");
+                int imageResource = data.getIntExtra("image-resource", 0);
+
+                //Create a list item from the values
+                ListItem item = new ListItem(title, description, imageResource);
+
+                //Add the new item to the adapter;
+                items.add(item);
+
+                //Have the adapter update
+                adapter.notifyDataSetChanged();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
