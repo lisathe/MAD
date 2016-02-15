973970f49176b4ade5c12c302ee2723d7c14fec1
Lisa The <lisathe97@gmail.com>
2016-02-15 16:08:11 +0100

    2.1: Making the activity ready:

--- Opdracht-2.3-ImageViewerAdvanced/AnimalViewer/app/src/main/java/com/example/lisa/animalviewer/FragmentActivity.java
+++ Opdracht-2.3-ImageViewerAdvanced/AnimalViewer/app/src/main/java/com/example/lisa/animalviewer/FragmentActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class FragmentActivity extends AppCompatActivity {
 
+    private int[] imageIds = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+        String[] animalNames = getResources().getStringArray(R.array.image_title);
     }
 }
