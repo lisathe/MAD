1905c06b91a64136f1d81ce492068f1d95a8c8c4
Lisa The <lisathe97@gmail.com>
2016-02-20 23:19:59 +0100

    Add filter dropdown menu

--- What2Wear/What2Wear/app/src/main/java/com/example/lisa/what2wear/FilterActivity.java
+++ What2Wear/What2Wear/app/src/main/java/com/example/lisa/what2wear/FilterActivity.java
@@ -6,8 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilterActivity extends AppCompatActivity {
+
+    MultiSelectionSpinner spinnerColor;
+    MultiSelectionSpinner spinnerBottom;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,33 @@ public class FilterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_filter);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        spinnerColor=(MultiSelectionSpinner)findViewById(R.id.color);
+        spinnerBottom=(MultiSelectionSpinner)findViewById(R.id.bottom);
+
+        //Color list
+        List<String> colors = new ArrayList<String>();
+        colors.add("Black");
+        colors.add("White");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Red");
+        colors.add("Yellow");
+        colors.add("Purple");
+        colors.add("Pink");
+        colors.add("Grey");
+        spinnerColor.setItems(colors);
+
+        //Bottom list
+        List<String> bottom = new ArrayList<String>();
+        bottom.add("Pants");
+        bottom.add("Skirts");
+        bottom.add("Leggings");
+        bottom.add("Shorts");
+        spinnerBottom.setItems(bottom);
+
+
+
     }
 
 }
